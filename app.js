d5c7c3ff08335e3c99aca8d896880fc1a496166a
dysnasia <64567523+dysnasia@users.noreply.github.com>
2024-02-03 19:06:12 +0000

    debugging

--- app.js
+++ app.js
@@ -3,19 +3,26 @@
 const video = document.getElementById('qr-video');
 const resultContainer = document.getElementById('qr-result');
 
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    resultContainer.textContent = `Scan result: ${decodedText}`;
+    // Stop the scanner once you got the result to prevent multiple readings
+    scanner.stop();
+}
+
+function onScanError(errorMessage) {
+    // handle scan error
+    console.error(errorMessage);
+    resultContainer.textContent = 'Error scanning QR Code: ' + errorMessage;
+}
+
 // Check if QrScanner is defined
 if (typeof QrScanner !== 'undefined') {
     // Set the path to the worker script
     QrScanner.WORKER_PATH = 'qr-scanner-worker.min.js';
 
-    // Create a new QrScanner instance
-    const scanner = new QrScanner(video, result => {
-        console.log('decoded qr code:', result);
-        resultContainer.textContent = `Scan result: ${result}`;
-        // Here you can handle the scan result if needed
-    });
+    const scanner = new QrScanner(video, onScanSuccess, onScanError);
 
-    // Start the scanner
     scanner.start().catch(e => {
         console.error(e);
         resultContainer.textContent = 'Error starting scanner: ' + e;
