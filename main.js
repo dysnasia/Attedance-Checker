265d449e267fc09de2303a22fa661e98973d876c
dysnasia <64567523+dysnasia@users.noreply.github.com>
2024-02-03 21:35:23 +0000

    fix

--- main.js
+++ main.js
@@ -1,46 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     const resultContainer = document.getElementById('qr-result');
-    const resultList = document.getElementById('result-list');
-    let scannedCodes = new Set(); // Set to store unique scanned codes
-
+    const resultList = document.getElementById('result-list'); // List to display results
+  
     function onScanSuccess(decodedText, decodedResult) {
-        // Check if the code has already been scanned
-        if (scannedCodes.has(decodedText)) {
-            console.log(`Code is already scanned: ${decodedText}`);
-            return; // Do not proceed if the code has been scanned
-        }
-
-        // Add the new unique code to the set
-        scannedCodes.add(decodedText);
-
-        // Create a new list item for the new unique code
+        // Create a new list item
         const newItem = document.createElement('li');
-        newItem.textContent = decodedText;
-
+        newItem.textContent = decodedText; // Set the text of the list item to the QR code text
+  
         // Append the new item to the result list
         resultList.appendChild(newItem);
-
+  
         // Optionally, clear the scanner if you want to stop scanning after a successful scan
-        // html5QrcodeScanner.clear();
+        html5QrcodeScanner.clear();
     }
-
-    function onScanError(errorMessage) {
-        resultContainer.textContent = `Error scanning QR Code: ${errorMessage}`;
-    }
-
+  
     // This method will trigger user permissions
     Html5Qrcode.getCameras().then(devices => {
         if (devices && devices.length) {
             var cameraId = devices[0].id;
-            // Prefer the rear camera if available
+            // Choose the rear camera if available
             devices.forEach(device => {
                 if (device.label && device.label.includes('back')) {
                     cameraId = device.id;
                 }
             });
-
-            const html5QrcodeScanner = new Html5Qrcode("reader", { fps: 30, qrbox: 250 });
-            html5QrcodeScanner.start(cameraId, {}, onScanSuccess, onScanError)
+  
+            const html5QrcodeScanner = new Html5Qrcode("reader", { fps: 20, qrbox: 250 });
+            html5QrcodeScanner.start(cameraId, {}, onScanSuccess)
                 .catch(err => {
                     resultContainer.textContent = `Unable to start QR scanner: ${err}`;
                 });
@@ -48,4 +34,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }).catch(err => {
         resultContainer.textContent = 'Error getting camera devices: ' + err;
     });
-});
+  });
+  
